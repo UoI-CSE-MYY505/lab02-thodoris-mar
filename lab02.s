83a5f185d086cb115912750b41c09dfc973fc8fd
thodoris-mar <cs05426@uoi.gr>
2024-10-19 13:44:43 +0300

    Update lab02.s

--- lab02.s
+++ lab02.s
@@ -9,10 +9,21 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a1, 7    # unsigned
     li a2, 1
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+   beq a1, zero, done  # If array size is 0, exit
+
+loop:
+    lw t0, 0(a0)         # Load the current element from memory
+    beq t0, a2, match    # If it matches the search value, store the address
+    addi a0, a0, 4       # Move to the next element's address
+    addi a1, a1, -1      # Decrease the remaining element count
+    bnez a1, loop        # If more elements remain, continue the loop
+    j done           
+
+match:
+    mv s0, a0           # Store the address of the matching element in s0
+    addi a0, a0, 4      # Move to the next element's address
+    addi a1, a1, -1     # Decrease the remaining element coun
+    bnez a1, loop       # If more elements remain, continue the loop
 done:
     addi a7, zero, 10 
     ecall
